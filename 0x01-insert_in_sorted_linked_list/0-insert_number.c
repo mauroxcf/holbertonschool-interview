0e3e4187bb69401488ec80b8c0718dcdddcbc632
mauroxcf <maurox03@gmail.com>
2021-07-27 02:27:11 -0500

    task 0 insert new node in a sorted linked list v3 fix betty

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -3,45 +3,46 @@
 /**
  * insert_node - insert a node in a listint_t list
  * @head: pointer to pointer of first node of listint_t list
- * @n: integer to be included in new node
+ * @number: integer to be included in new node
  * Return: address of the new element or NULL if it fails
  */
-listint_t *insert_node(listint_t **head, int number) {
+listint_t *insert_node(listint_t **head, int number)
+{
 
-    listint_t *new_node;
-    listint_t *current;
+	listint_t *new_node;
+	listint_t *current;
 
-    current = *head;
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-        return (NULL);
+	current = *head;
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    if (head == NULL || *head == NULL)
+	if (head == NULL || *head == NULL)
 	{
 		*head = new_node;
 		return (new_node);
 	}
 
-    new_node->n = number;
-    new_node->next = NULL;
+	new_node->n = number;
+	new_node->next = NULL;
 
-    if (current->n >= number)
+	if (current->n >= number)
 	{
 		new_node->next = current;
 		*head = new_node;
 		return (new_node);
 	}
 
-    while (current->next != NULL)
-    {
-        if (new_node->n <= current->next->n)
-        {
-            new_node->next = current->next;
-            current->next = new_node;
-            break;
-        }
-        current = current->next;
-    }
+	while (current->next != NULL)
+	{
+	if (new_node->n <= current->next->n)
+	{
+		new_node->next = current->next;
+		current->next = new_node;
+		break;
+	}
+	current = current->next;
+	}
 
-    return (new_node);
+	return (new_node);
 }
