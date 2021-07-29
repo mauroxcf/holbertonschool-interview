ebba4b9049f8ccd79804eaf7b8d677e041f51ab6
mauroxcf <maurox03@gmail.com>
2021-07-29 00:31:33 -0500

    task 0 v6

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -10,15 +10,12 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *new_node, *current, *temp;
 
-	current = *head;
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = number;
 	new_node->next = NULL;
-	temp = *head;
-	current = temp;
 
 	if (head == NULL || *head == NULL)
 	{
@@ -26,9 +23,12 @@ listint_t *insert_node(listint_t **head, int number)
 		return (new_node);
 	}
 
+	temp = *head;
+	current = temp;
+
 	if (current->n >= number)
 	{
-		new_node->next = current;
+		new_node->next = temp;
 		*head = new_node;
 		return (new_node);
 	}
@@ -41,6 +41,7 @@ listint_t *insert_node(listint_t **head, int number)
 			current->next = new_node;
 			return (new_node);
 		}
+		current = temp;
 		temp = temp->next;
 	}
 
