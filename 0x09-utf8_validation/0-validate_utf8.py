df5ea4dfe0f08347ad600b5ec6dca2a581e60265
mauroxcf <maurox03@gmail.com>
2021-09-23 01:24:12 -0500

    task 0 validate unicode

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -7,11 +7,28 @@ def validUTF8(data):
     Args:
         data (list): contain multiple characters
     """
-    if len(data) > 32:
-        return False
+     n_bytes = 0
 
-    for i in data:
-        if i > 255:
-            return False
+    for num in data:
+        bin_rep = format(num, '#010b')[-8:]
 
-    return True
+        if n_bytes == 0:
+
+            for bit in bin_rep:
+                if bit == '0':
+                    break
+                n_bytes += 1
+
+            if n_bytes == 0:
+                continue
+
+            if n_bytes == 1 or n_bytes > 4:
+                return False
+
+        else:
+            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
+                return False
+
+        n_bytes -= 1
+
+    return n_bytes == 0
